55f6d3ff9fb2c01a4a4e365ced80f11f66218d4d
48s97j7ht5-ux <48s97j7ht5@privaterelay.appleid.com>
2026-08-11 13:52:04 +0300

    Add v0.6 character anchors

---
+++ character/data/anchors.js
@@ -0,0 +1,24 @@
+export const BASE={cx:60,headY:14};
+
+export const ANCHORS={
+  headNeck:{x:60,y:14},
+  torsoUpper:{x:60,y:31},
+  torsoMid:{x:60,y:44},
+  pelvisCore:{x:60,y:56},
+  upperArmL:{x:47,y:35},
+  upperArmR:{x:73,y:35},
+  forearmL:{x:45,y:45},
+  forearmR:{x:75,y:45},
+  thighL:{x:54,y:68},
+  thighR:{x:66,y:68},
+  lowerLegL:{x:54,y:80},
+  lowerLegR:{x:66,y:80},
+  footL:{x:53,y:94},
+  footR:{x:67,y:94}
+};
+
+export const LAYERS=[
+  'backHair','headNeck','upperArmL','upperArmR','forearmL','forearmR',
+  'torsoUpper','torsoMid','thighL','thighR','lowerLegL','lowerLegR','footL','footR',
+  'pelvisCore','bodyShading','underwearTop','underwearBottom','face','frontHair'
+];
