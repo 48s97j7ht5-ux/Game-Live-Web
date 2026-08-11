12be3e1d5230e4c94996112a802f5fd1eeb4bad0
48s97j7ht5-ux <48s97j7ht5@privaterelay.appleid.com>
2026-08-11 13:52:10 +0300

    Add v0.6 character preset

---
+++ character/data/presets.js
@@ -0,0 +1,9 @@
+export const DEFAULT={height:168,shoulders:100,waist:100,hips:100,skin:28};
+
+export const LIMITS={
+  height:[155,190],
+  shoulders:[80,120],
+  waist:[82,118],
+  hips:[86,124],
+  skin:[0,100]
+};
